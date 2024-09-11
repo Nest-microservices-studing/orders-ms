1aa6d7550f88c99f296b94287191c1325c2d8eda
PabloAndres2000 <l.pablo4125@gmail.com>
2024-09-11 00:15:59 -0300

    Se crea conexion NATS y se hace que sea un array, para que se ingrese en el archivo .env una lista de conexiones NATS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,35 +1,21 @@
 import 'dotenv/config';
 import * as joi from 'joi';
 
-// Interfaz para las configuraciones de los microservicios
-interface MicroserviceConfig {
-    PORT: number;
-    HOST: string;
-}
-
 interface EnvVars {
     PORT: number;
-    PRODUCTS_MICROSERVICE: MicroserviceConfig;
-}
 
-// Schema de validación de las variables de entorno usando Joi
-const microserviceSchema = joi.object({
-    PORT: joi.number().required(),
-    HOST: joi.string().required(),
-});
+    NATS_SERVERS: string[];
+}
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
-    PRODUCTS_MICROSERVICE: microserviceSchema.required(),
-}).unknown(true);
+    NATS_SERVERS: joi.array().items(joi.string()).required()
+})
+.unknown(true);
 
-// Validación de las variables de entorno
-const { error, value } = envsSchema.validate({
-    PORT: process.env.PORT,
-    PRODUCTS_MICROSERVICE: {
-        PORT: process.env.PRODUCT_MICROSERVICE_PORT,
-        HOST: process.env.PRODUCT_MICROSERVICE_HOST,
-    },
+const {error, value} = envsSchema.validate({
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
 });
 
 if (error) {
@@ -38,11 +24,7 @@ if (error) {
 
 const envVars: EnvVars = value;
 
-// Exportación de las variables de entorno ya validadas y organizadas
 export const envs = {
     port: envVars.PORT,
-    productsMicroservice: {
-        host: envVars.PRODUCTS_MICROSERVICE.HOST,
-        port: envVars.PRODUCTS_MICROSERVICE.PORT,
-    },
-};
+    natsServers: envVars.NATS_SERVERS
+}
